c72ed39b6899fd1d98cea801ae19fc711c0ae941
Hussain Ali <huali235@gmail.com>
2023-11-29 17:44:57 -0500

    add loader, action functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,12 @@
+import { json, redirect } from "react-router-dom";
+import { createOrder, getMenu, getOrder } from "../services/apiRestaurant";
+
+// https://uibakery.io/regex-library/phone-number
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str
+  );
+
 export function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
     style: "currency",
@@ -18,4 +27,36 @@ export function calcMinutesLeft(dateStr) {
   const d1 = new Date().getTime();
   const d2 = new Date(dateStr).getTime();
   return Math.round((d2 - d1) / 60000);
+}
+
+export async function menuLoader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export async function orderLoader({ params }) {
+  const order = await getOrder(params.orderId);
+  return order;
+}
+
+export async function createOrderAction({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      "This is not a valid number. Please enter a valid phone number.";
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  const newOrder = await createOrder(order);
+
+  return redirect(`/order/${newOrder.id}`);
 }
