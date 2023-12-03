8778e8ad90a8488cf6404073e1fa865d44f965aa
Hussain Ali <huali235@gmail.com>
2023-12-02 20:12:56 -0500

    change currency to USD

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,25 +1,25 @@
-import { json, redirect } from "react-router-dom";
-import { createOrder, getMenu, getOrder } from "../services/apiRestaurant";
+import { redirect } from 'react-router-dom';
+import { createOrder, getMenu, getOrder } from '../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 export function formatCurrency(value) {
-  return new Intl.NumberFormat("en", {
-    style: "currency",
-    currency: "EUR",
+  return new Intl.NumberFormat('en', {
+    style: 'currency',
+    currency: 'USD',
   }).format(value);
 }
 
 export function formatDate(dateStr) {
-  return new Intl.DateTimeFormat("en", {
-    day: "numeric",
-    month: "short",
-    hour: "2-digit",
-    minute: "2-digit",
+  return new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'short',
+    hour: '2-digit',
+    minute: '2-digit',
   }).format(new Date(dateStr));
 }
 
@@ -46,13 +46,13 @@ export async function createOrderAction({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === 'on',
   };
 
   const errors = {};
   if (!isValidPhone(order.phone))
     errors.phone =
-      "This is not a valid number. Please enter a valid phone number.";
+      'This is not a valid number. Please enter a valid phone number.';
 
   if (Object.keys(errors).length > 0) return errors;
 
